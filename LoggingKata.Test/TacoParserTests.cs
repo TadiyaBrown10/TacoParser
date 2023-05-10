280a492037b1692c0f3d7ce83c91ab090ef2a447
Tadiya Brown <tadiyabrown2000@gmail.com>
2023-05-10 13:53:15 -0500

    final commit

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -35,14 +35,27 @@ namespace LoggingKata.Test
 
             //Act
 
-            var actual = TacoParserInstance.Parse(line).Location.Longitude;
+            var actual = TacoParserInstance.Parse(line);
 
             //Assert
-            Assert.Equal(expected, actual);
+            Assert.Equal(expected, actual.Location.Longitude);
         }
 
 
         //TODO: Create a test ShouldParseLatitude
+        [Theory]
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.073638)]
+        public void ShouldParseLatitude(string line, double expected)
+        {
 
+            var TacoParserInstance = new TacoParser();
+
+            
+
+            var actual = TacoParserInstance.Parse(line);
+
+            
+            Assert.Equal(expected, actual.Location.Latitude);
+        }
     }
 }
